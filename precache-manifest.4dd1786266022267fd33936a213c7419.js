7867c8ce2511dc72a731d5343c8617ef664d7fdc
SamiranWaghmare <samwagh81@email.com>
2022-08-05 11:31:08 +0530

    testing out if I know how to use git

---
+++ precache-manifest.4dd1786266022267fd33936a213c7419.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffb74ad057f8634bd175",
+    "url": "/robofriends-by-andrei/static/css/main.566b0eb2.chunk.css"
+  },
+  {
+    "revision": "ffb74ad057f8634bd175",
+    "url": "/robofriends-by-andrei/static/js/main.ffb74ad0.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends-by-andrei/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "3b4260a4ee2e549830c3",
+    "url": "/robofriends-by-andrei/static/css/2.e17facab.chunk.css"
+  },
+  {
+    "revision": "3b4260a4ee2e549830c3",
+    "url": "/robofriends-by-andrei/static/js/2.3b4260a4.chunk.js"
+  },
+  {
+    "revision": "6c361679d71fb59e036f",
+    "url": "/robofriends-by-andrei/static/js/runtime~main.6c361679.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-by-andrei/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7a8b1fedc85d9432aaa84ffa09b40066",
+    "url": "/robofriends-by-andrei/index.html"
+  }
+];
